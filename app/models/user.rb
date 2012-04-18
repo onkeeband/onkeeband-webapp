074e0bfbeb0c7b5bcf75136a52d344c4c37f980b
Antwan Foster <Antwan3000@gmail.com>
2012-04-18 00:38:59 -0400

    Updated for devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,25 @@ class User
   devise :database_authenticatable, :registerable, :timeoutable,
          :recoverable, :rememberable, :trackable, :validatable
   
-  field :name
+  # Field Names
+  field :name,    :type => String, :null => false
+  
+  # Devise Field Names
+  ## Database authenticatable
+  field :email,              :type => String, :null => false
+  field :encrypted_password, :type => String, :null => false
+  ## Recoverable
+  field :reset_password_token,   :type => String
+  field :reset_password_sent_at, :type => Time
+  ## Rememberable
+  field :remember_created_at, :type => Time
+  ## Trackable
+  field :sign_in_count,      :type => Integer
+  field :current_sign_in_at, :type => Time
+  field :last_sign_in_at,    :type => Time
+  field :current_sign_in_ip, :type => String
+  field :last_sign_in_ip,    :type => String
+    
   validates_presence_of :name
   validates_uniqueness_of :name, :email, :case_sensitive => false
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
