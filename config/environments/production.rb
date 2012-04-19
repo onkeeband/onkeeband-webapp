f94858724d9c67b6f87321d5ef6b840199d92b7f
Antwan Foster <Antwan3000@gmail.com>
2012-04-19 13:58:48 -0400

    Added assets to precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,7 @@ Onkeeband::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( audo-player.js jwplayer.js swfobject.js tabbedContent.js google_analytics.js galleria.js galleria.lightbox.js galleria.lightbox.css jquery.nivo.slider.pack.js nivo-slider.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
